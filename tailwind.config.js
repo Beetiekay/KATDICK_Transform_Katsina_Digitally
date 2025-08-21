dc87b23dd807b5b85288cd082b3f6b691e5648b0
bishir tukur <bastukur1997@gmail.com>
2025-08-21 19:27:10 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+],
+theme: {
+  extend: {
+    borderColor: theme => ({
+      DEFAULT: theme('color.gray,300', 'currentColor'),
+      'gray':'#8fbc8f',
+    }),
+    borderColor: theme => ({
+      'gray':'#a9a9a9',
+    }),
+  },
+  varients: {
+    extend:{
+      backgroundColor: ['checked'],
+    },
+  },
+   colors:{
+   'gray':'#a9a9a9',
+   'black':'#000000',
+   'seagreen':'#009832',
+   'green':'#2e9d17',
+   'white':'#fffafa',
+   'whitegray':'#ffffff',
+   'cobalt':'#244aa4',
+   'royalblue':'#3469e9',
+   'midnightblue':'#1f214c',
+   'hack':'#2d6776',
+   'carrot':'#f89c1c',
+  },
+
+},
+plugins: [],
+}
+
